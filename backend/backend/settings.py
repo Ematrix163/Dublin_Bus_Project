e7795311d3c8e949077b0507a06aed02ae8d0ec6
thanders <thanders@alumni.purdue.edu>
2018-07-10 15:36:08 +0100

    export scripts updated, Django backend files modified so UCDVM can serve

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '&b5fsz^0&^kbxla3ku9)#rn+1x$yp+1_-#uq)09c#$a$wn-zy9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['csi420-01-vm2.ucd.ie', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -87,7 +87,7 @@ DATABASES = {
         'USER': 'front_end',
         'PASSWORD': '12345',
 		'HOST': 'localhost',
-		'PORT': '3333'
+		'PORT': '3306'
     }
 }
 
