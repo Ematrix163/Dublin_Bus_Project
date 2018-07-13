95915a4e6b3a5dc9194c7b97c2398a7abb6c5fef
thanders <thanders@alumni.purdue.edu>
2018-07-13 15:06:09 +0100

    removed comments

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,6 @@ from django.urls import path
 
 
 from api.views import RouteIdView, RoutesStopidView
-""" Added by Tom """
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
