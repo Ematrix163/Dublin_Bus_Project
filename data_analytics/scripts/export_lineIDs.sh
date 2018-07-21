a19d32b84c8f955aa82ddb3c51b7643e53dfe5cb
thanders <thanders@alumni.purdue.edu>
2018-07-21 12:05:53 +0100

    Commit

--- data_analytics/scripts/export_lineIDs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pathDf="/home/student/data/db_historic/rt_trips_2017_I_DB.txt"
-
-# Use awk to sort pathDf and append values to bus_lines.csv
-awk -F";" '!_[$4]++' $pathDf | cut -d';' -f4 | grep . >> bus_lines.csv
-
--- data_analytics/scripts/job_master.sh
+++ data_analytics/scripts/job_master.sh
@@ -3,17 +3,17 @@
 
 extract_lineIDs="/home/student/data_analytics/scripts/extract_lineIDs.sh"
 extract_bus_line="/home/student/data_analytics/scripts/extract_bus_line.sh"
-temp_path="/home/student/data_analytics/prediction_model/extracted/tmp/"
-lineID_file="$temp_path/bus_lineIDs.csv"
-merge_finished="$temp_path/merge_done.csv"
+temp_path="/home/student/data_analytics/prediction_model/extracted/tmp"
+lineID_file="$temp_path/bus_lineIDs.txt"
+merge_done="$temp_path/merge_done.txt"
 
 # Make directory (-p does not output error if it already exists)
 mkdir -p $temp_path
-touch $merge_finished
-    if [ -f $lineID_file ]
-    then
-        rm -f $lineID_file;
-    fi
+
+# If LineID_file exists, remove it
+if [ -f $lineID_file ]; then
+    rm -f $lineID_file;
+fi
 
 # Run the extract_lineIDs.sh script (as another process)
 bash $extract_lineIDs ; reply=$?
@@ -34,19 +34,29 @@ else
     # Delete the LineID file
     rm $lineID_file
 
-    echo "testing:"
-    for index in "${a[@]}";
-    do
-        echo $index
-        if grep -F "$index" $merge_finished; then
-            echo "Found!"
-            exit 1
-        else
-            bash $extract_bus_line $index
-            echo "Horray!"
+    count=0
 
+    echo "testing:"
+    for elem in "${a[@]}"; do
+
+        echo "Element in array: $elem"
+
+        # If element is found in the merge_finished file then:
+
+        if [ "$(fgrep '$elem' $merge_done)" -ge 1 ]; then
+            echo "Found $elem in file"
+            count=$(($count+1))
+            echo "count: $count"
+            if [ $count -ge 5 ]; then
+                exit
+            fi
+        else
+            echo "Extracting bus line"
+            bash $extract_bus_line $elem
+            echo "$elem" >> $merge_done
 
         fi
+    echo "done"
     done
     # Print out all elements in array
     # echo "${a[@]}"
