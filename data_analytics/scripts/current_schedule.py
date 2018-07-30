c70c1c099a69362aef1a6f4f85091ca9cff512bb
thanders <thanders@alumni.purdue.edu>
2018-07-30 21:51:30 +0100

    Added functionality for downloading and unzipping gtfs files

--- data_analytics/scripts/current_schedule.py
+++ data_analytics/scripts/current_schedule.py
@@ -1,10 +1,16 @@
+# -*- coding: utf-8 -*
+
 import pandas as pd
 import os
 import shutil
 import string
 
 import subprocess
-import pexpect
+import signal
+
+
+import wget
+import zipfile
 import sys
 
 
@@ -14,6 +20,8 @@ class CurrentSchedule:
     _mysql_load_command = "bash /home/student/data_analytics/scripts/mysql_load_data.sh"
     _output_path = "/home/student/data_analytics/prediction_model/tmp/"
     _output_file = "dublinBus_schedule_current.csv"
+    _schedule_path = '/storage/current_schedule/'
+    _schedule_file = _schedule_path+'google_transit_dublinbus.zip'
 
     # This method automatically initializes the class variables when an class instance is created
     def __init__(self, input_path_one, input_path_two):
@@ -39,8 +47,6 @@ class CurrentSchedule:
         # merge with the main df basedhttps://stackoverflow.com/questions/23441994/truncate-table-via-command-line-in-linux on year and country
         self._df_merged = pd.merge(self._df_one, self._df_two, how='left', left_on=['trip_id'], right_on=['trip_id'])
 
-
-
     # Creates line_id col in dataframe
     def create_line_id(self):
         self._df_merged['line_id'] = self._df_merged['route_id']
@@ -62,7 +68,7 @@ class CurrentSchedule:
             print ("Creation of directory %s failed" % self._output_path)
         else:
             print ("Successfully created directory %s " % self._output_path)
-
+    # delete temporary files that are associated with the merge and database upload
     def delete_tmp(self):
 
         ## If file exists, delete it ##
@@ -80,9 +86,36 @@ class CurrentSchedule:
     # Calls a shell script to get mysql to truncate table and load data in file
     def mysql_load_data(self):
         #    mysqlimport --ignore-lines=1 --fields-terminated-by=, --verbose --local -u [user] -p [database] /path/to/address.csv
-        subprocess.Popen([ self._mysql_load_command ], shell=True)
+        #subprocess.Popen([ self._mysql_load_command ], shell=True)
+        process = subprocess.Popen([ self._mysql_load_command ], stdout=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        stdout = process.communicate()[0]
+        print ('STDOUT:{}'.format(stdout))
+        # os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+        print("loading data finished")
 
-# Schedule File paths
+    def download(self):
+        print('Beginning file download with wget module')
+        url = 'http://www.transportforireland.ie/transitData/google_transit_dublinbus.zip'
+        wget.download(url, '/storage/current_schedule/')
+
+    def unzip(self):
+        print('Unzipping file')
+        print(self._schedule_path)
+        if os.path.exists(self._schedule_file):
+            with zipfile.ZipFile(self._schedule_file, "r") as zip_ref:
+                zip_ref.extractall(self._schedule_path)
+
+    # Delete gtfs files before downloading the new ones.
+    def delete_gtfs_files(self):
+
+        ## If file exists, delete it ##
+        if os.path.isdir(self._schedule_path):
+            shutil.rmtree(self._schedule_path, ignore_errors=True)
+            print("Removed files in folder %s" % self._schedule_path)
+        else:  ## Show an error ##
+            print("Note: %s Do not need to remove any temporary files" % self._schedule_path)
+
+    # Schedule File paths
 current_stop_times = "/storage/current_schedule/stop_times.txt"
 current_trips = "/storage/current_schedule/trips.txt"
 
@@ -90,15 +123,36 @@ current_trips = "/storage/current_schedule/trips.txt"
 # Create instance
 instance = CurrentSchedule(current_stop_times, current_trips)
 
+
+# Delete temporary files
 instance.delete_tmp()
+instance.delete_gtfs_files()
+
+# Download GTFS current schedule files from DublinBus
+instance.download()
+
+# Unzip downloaded files
+instance.unzip()
+
+# Create dataframes
 instance.create_data_frames()
+
+# Merge dataframes
 instance.merge()
+
+#perform cleaning operations
+
 instance.create_line_id()
 instance.modify_stop_id()
 instance.clean_columns()
+
+#
 instance.create_directory()
 instance.export()
 instance.mysql_load_data()
-#instance.delete_tmp()
+instance.delete_tmp()
+instance.delete_gtfs_files()
+
+
 
 
