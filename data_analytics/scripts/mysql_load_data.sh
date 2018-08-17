eaf3db5ddb6d4681b7103e9a2d9fee6fffb6df07
thanders <thanders@alumni.purdue.edu>
2018-08-17 23:02:53 +0100

    removal

--- data_analytics/scripts/mysql_load_data.sh
+++ data_analytics/scripts/mysql_load_data.sh
@@ -6,9 +6,9 @@ table_name="dublinBus.dublinBus_schedule_current"
 log=/home/student/data_analytics/scripts/logs/mysql_load_data.log
 
 echo "Truncating table $table_name"
-mysql -u front_end -p'12345' dublinBus -e "TRUNCATE $table_name"
+mysql -u front_end -p'password' dublinBus -e "TRUNCATE $table_name"
 echo "Loading data into $table_ename"
-mysql -u front_end -p'12345' --execute="LOAD DATA INFILE '$schedule_path' INTO TABLE $table_name FIELDS TERMINATED BY ',' IGNORE 1 LINES; SHOW WARNINGS"
+mysql -u front_end -p'password' --execute="LOAD DATA INFILE '$schedule_path' INTO TABLE $table_name FIELDS TERMINATED BY ',' IGNORE 1 LINES; SHOW WARNINGS"
 echo "End of script"
 exit
 
